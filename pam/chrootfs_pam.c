bba292d89f21e00db12b8ca736446e125614f702
Jan Nidzwetzki <kn@achfrag.net>
2014-11-05 07:39:07 +0100

    Added Pam skeleton

--- pam/chrootfs_pam.c
+++ pam/chrootfs_pam.c
@@ -1,2 +1,11 @@
 #include <security/pam_appl.h>
 
+#define  PAM_SM_AUTH
+#define  PAM_SM_ACCOUNT
+#define  PAM_SM_SESSION
+
+int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+	return(PAM_IGNORE);
+}
+
+

93ad848833cd4dc9953570306f31d8a742aec1f4
Jan Nidzwetzki <kn@achfrag.net>
2014-11-05 12:21:12 +0100

    Added missing license header

--- pam/chrootfs_pam.c
+++ pam/chrootfs_pam.c
@@ -1,8 +1,27 @@
+/*
+    Copyright 2014 Jan Nidzwetzki
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+*/
+
+#include <stdio.h>
+#include <unistd.h>
 #include <security/pam_appl.h>
 
-#define  PAM_SM_AUTH
-#define  PAM_SM_ACCOUNT
-#define  PAM_SM_SESSION
+#define PAM_SM_AUTH
+#define PAM_SM_ACCOUNT
+#define PAM_SM_SESSION
 
 int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
 	return(PAM_IGNORE);

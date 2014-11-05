2101710608c3b7ad5429119bcfa9da635701b78d
Jan Nidzwetzki <kn@achfrag.net>
2014-11-05 12:56:09 +0100

    PAM Snapshot

--- pam/chrootfs_pam.c
+++ pam/chrootfs_pam.c
@@ -17,13 +17,63 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <syslog.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <security/pam_appl.h>
 
 #define PAM_SM_AUTH
 #define PAM_SM_ACCOUNT
 #define PAM_SM_SESSION
 
-int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+#define PAM_CHROOT_ERROR -1
+
+#define CHROOTFS_DIR "/var/chroot"
+
+void chrootfs_pam_log(int err, const char *format, ...)
+{
+	va_list params;
+
+	va_start(params, format);
+	openlog("pam_chrootfs", LOG_PID, LOG_AUTHPRIV);
+	vsyslog(err, format, params);
+	va_end(params);
+	closelog();
+}
+
+bool chroot_dir_exists()
+{
+	int err;
+	struct stat s;
+
+	err = stat(CHROOTFS_DIR, &s);
+
+	if(err != -1)
+		if(S_ISDIR(s.st_mode))
+			return true;
+
+	return false;
+}
+
+int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) 
+{
+	int err;
+	char *username;
+
+	err = pam_get_user(pamh, &username, NULL);
+
+	if(err != PAM_SUCCESS) {
+		chrootfs_pam_log(LOG_ERR, "pam_chrootfs: unable to read username");
+		return PAM_CHROOT_ERROR;
+	}
+
+	if(chroot_dir_exists() != true) {
+		chrootfs_pam_log(LOG_ERR, "pam_chrootfs: unable to open chroot dir %s", CHROOTFS_DIR);
+		return PAM_CHROOT_ERROR;
+	}
+
+
 	return(PAM_IGNORE);
 }
 

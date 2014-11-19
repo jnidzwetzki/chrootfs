8de52d604feb600f96843546f0732a4f52cf7aff
Jan Nidzwetzki <kn@achfrag.net>
2014-11-19 08:56:28 +0100

    Added commandline parsing

--- chrootfsmng/chrootfsmng.c
+++ chrootfsmng/chrootfsmng.c
@@ -18,8 +18,128 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <libchrootfs.h>
+
+// Defines
+#define FLAG_MOUNT   1
+#define FLAG_UMOUNT  2
+#define FLAG_ENABLE  4
+#define FLAG_DISABLE 8
+
+// Structs
+typedef struct configuration {
+	unsigned int flags;
+	char* username;
+} configuration;
+
+void print_help_and_exit(const char *name)
+{
+	printf("Usage %s [Options] username\n", name);
+	printf("\n");
+	printf("Options:\n");
+	printf("-e - Enable chrootfs for user\n");
+	printf("-d - Disable chrootfs for user\n");
+	printf("-m - Mount chrootfs for user\n");
+	printf("-u - Umount chrootfs for user\n");
+	printf("-h - Print this help\n");
+	printf("\n");
+	
+	exit(EXIT_SUCCESS);
+}
+
+void parse_commandline_args(int argc, char *argv[], configuration *config)
+{
+	int option = 0;
+	unsigned int flags = 0;
+
+	while ((option = getopt(argc, argv,"m:u:e:d:h")) != -1) {
+		switch (option) {
+			case 'm':
+				flags |= FLAG_MOUNT;
+				config->username = optarg;
+			break;
+			
+			case 'u':
+				flags |= FLAG_UMOUNT;
+				config->username = optarg;
+			break;
+			
+			case 'e':
+				flags |= FLAG_ENABLE;
+				config->username = optarg;
+			break;
+			
+			case 'd':
+				flags |= FLAG_DISABLE;
+				config->username = optarg;
+			break;
+
+			case 'h':
+				print_help_and_exit(argv[0]);
+			break;
+
+			default:
+				printf("Unknown option %c\n\n", option);
+				print_help_and_exit(argv[0]);
+			break;
+		}
+	}
+
+	// Check for invalid flag combination
+	if(flags != FLAG_MOUNT || flags != FLAG_UMOUNT || 
+	   flags != FLAG_ENABLE || flags != FLAG_DISABLE) {
+		printf("Invalid combination of flags\n");
+		print_help_and_exit(argv[0]);
+	}
+
+	config->flags = flags;
+}
+
+void mount_filesystem(configuration *config)
+{
+
+}
+
+void umount_filesystem(configuration *config)
+{
+
+}
+
+void enable_chrootfs(configuration *config)
+{
+
+}
+
+void disable_chrootfs(configuration *config)
+{
+
+}
 
 int main(int argc, char *argv[])
 {
+	configuration config;
+	parse_commandline_args(argc, argv, &config);
+
+	switch(config.flags) {
+		case FLAG_MOUNT:
+			mount_filesystem(&config);
+		break;
+
+		case FLAG_UMOUNT:
+			umount_filesystem(&config);
+		break;
+
+		case FLAG_ENABLE:
+			enable_chrootfs(&config);
+		break;
+
+		case FLAG_DISABLE:
+			disable_chrootfs(&config);
+		break;
+
+		default:
+			print_help_and_exit(argv[0]);
+	}
+
 	return EXIT_SUCCESS;
 }
